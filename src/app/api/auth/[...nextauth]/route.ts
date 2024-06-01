157c68acf7283ff2735e307af881d79fce311fe7
Miguel Martínez <leugimmanre@gmail.com>
2024-06-01 22:22:29 +0200

    CredentialsProvider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import CredentialsProvider  from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
@@ -7,7 +7,7 @@ const prisma = new PrismaClient();
 
 const handlers = NextAuth({
   providers: [
-    CredentialsProvider ({
+    CredentialsProvider({
       name: "Credentials",
       credentials: {
         email: {
@@ -19,7 +19,7 @@ const handlers = NextAuth({
           label: "Password",
           type: "password",
           placeholder: "******",
-        }
+        },
       },
       async authorize(credentials) {
         if (!credentials || !credentials.email || !credentials.password) {
@@ -32,46 +32,22 @@ const handlers = NextAuth({
           });
 
           if (user && bcrypt.compareSync(credentials.password, user.password)) {
-            return { id: user.id.toString(), name: user.username, email: user.email };
+            return {
+              id: user.id.toString(),
+              name: user.username,
+              email: user.email,
+            };
           }
         } catch (error) {
           console.error("Error in authorization:", error);
         }
         return null;
-
       },
-    }),
-
-    CredentialsProvider({
-      id: "1",
-      name: "admin",
-      credentials: {
-        email: { label: "Email", type: "text", placeholder: "admin@example.com" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials) {
-        if (!credentials || !credentials.email || !credentials.password) {
-          return null;
-        }
-
-        try {
-          const admin = await prisma.admin.findUnique({
-            where: { email: credentials.email }
-          });
-
-          if (admin && bcrypt.compareSync(credentials.password, admin.password)) {
-            return { id: admin.id.toString(), name: admin.username, email: admin.email, role: 'admin' };
-          }
-        } catch (error) {
-          console.error("Error in authorization:", error);
-        }
-        return null;
-      }
     }),
   ],
   callbacks: {
-    jwt: async ({account, token, user }) => {
-      console.log(account)
+    jwt: async ({ account, token, user }) => {
+      console.log(account);
       if (user) {
         token.user = user;
       }
@@ -85,9 +61,9 @@ const handlers = NextAuth({
     },
   },
   pages: {
-    signIn: '/user/login',
-    error: '/auth/error',
+    signIn: "/user/login",
+    error: "/auth/error",
   },
 });
 
-export {handlers as GET, handlers as POST}
+export { handlers as GET, handlers as POST };
