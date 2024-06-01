91b2c8cf7df6983679a5c91b61b270f6a2d02057
Miguel Martínez <leugimmanre@gmail.com>
2024-06-02 00:41:21 +0200

    Middleware default

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,55 +1,16 @@
-// export { default } from "next-auth/middleware";
-
-// export const config = {
-//   matcher: ["/products/:path*", "/orders/:path*", "/admin/:path*"],
-// };
-
-
-
-
-// import { withAuth } from "next-auth/middleware";
-
-// export default withAuth({
-//   pages: {
-//     signIn: '/user/login',
-//     error: '/auth/error'
-//   }
-// });
-
-// export const config = {
-//   matcher: ['/products', '/orders/:path*', '/admin/:path*']
-// };
-
-
-
-import { NextResponse, type NextRequest } from 'next/server'
-
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/user/login', request.url))
-}
+export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ['/products/:path*', '/orders/:path*', '/admin/:path*'],
-}
+  matcher: ["/products/:path*", "/orders/:path*", "/admin/:path*"],
+};
 
-
-
-
-// import { NextResponse } from 'next/server';
-// import type { NextRequest } from 'next/server';
+// import { NextResponse, type NextRequest } from 'next/server'
 
 // export function middleware(request: NextRequest) {
-//   // Assuming /user/login is the login route
-//   const loginUrl = new URL('/user/login', request.url);
-
-//   // Perform the redirection if not authenticated
-//   if (!request.nextUrl.pathname.startsWith('/user/login')) {
-//     return NextResponse.redirect(loginUrl);
-//   }
-
-//   return NextResponse.next();
+//   return NextResponse.redirect(new URL('/user/login', request.url))
 // }
 
 // export const config = {
 //   matcher: ['/products/:path*', '/orders/:path*', '/admin/:path*'],
-// };
+// }
+
