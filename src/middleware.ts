20519a6f378134dfe89ffc9d1c9f2485c4d53f28
Miguel Martínez <leugimmanre@gmail.com>
2024-06-01 23:50:54 +0200

    Middleware default

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,58 @@
-// export { default } from "next-auth/middleware";
-
-// export const config = {
-//   matcher: ["/products", "/orders/:path*", "/admin/:path*"],
-// };
-import { withAuth } from "next-auth/middleware";
-
-export default withAuth({
-  pages: {
-    signIn: '/user/login',
-    error: '/auth/error'
-  }
-});
+export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ['/products', '/orders/:path*', '/admin/:path*']
+  matcher: ["/products", "/orders/:path*", "/admin/:path*"],
 };
+
+
+
+
+// import { withAuth } from "next-auth/middleware";
+
+// export default withAuth({
+//   pages: {
+//     signIn: '/user/login',
+//     error: '/auth/error'
+//   }
+// });
+
+// export const config = {
+//   matcher: ['/products', '/orders/:path*', '/admin/:path*']
+// };
+
+
+
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
+
+// // This function can be marked `async` if using `await` inside
+// export function middleware(request: NextRequest) {
+//   return NextResponse.redirect(new URL('/user/login', request.url))
+// }
+
+// // See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: ['/products', '/orders/:path*', '/admin/:path*'],
+// }
+
+
+
+
+// import { NextResponse } from 'next/server';
+// import type { NextRequest } from 'next/server';
+
+// export function middleware(request: NextRequest) {
+//   // Assuming /user/login is the login route
+//   const loginUrl = new URL('/user/login', request.url);
+
+//   // Perform the redirection if not authenticated
+//   if (!request.nextUrl.pathname.startsWith('/user/login')) {
+//     return NextResponse.redirect(loginUrl);
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ['/products/:path*', '/orders/:path*', '/admin/:path*'],
+// };
